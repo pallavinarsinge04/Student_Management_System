47c83ae55463d184acad300eae485031b806bb37
pallavinarsinge04 <pallavinarsinge123@gmail.com>
2026-02-25 23:57:37 +0530

    Add virtual environment and update .gitignore

--- models.py
+++ models.py
@@ -8,6 +8,5 @@ class Student(db.Model):
     email = db.Column(db.String(120), nullable=False, unique=True)
     course = db.Column(db.String(100), nullable=False)
     marks = db.Column(db.Integer, nullable=False)
-
     def __repr__(self):
         return f"<Student {self.name}>"
